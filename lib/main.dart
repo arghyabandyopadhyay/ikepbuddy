af12ccbaf22b4d2f413106ef82e8a0b5eed9a24c
Arghya Bandyopadhyay <72153788+arghyabandyopadhyay@users.noreply.github.com>
2022-08-26 17:32:19 +0530

    update

--- lib/main.dart
+++ lib/main.dart
@@ -23,6 +23,7 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
   // If you're going to use other Firebase services in the background, such as Firestore,
   // make sure you call `initializeApp` before using other Firebase services.
   await Firebase.initializeApp();
+  print("firebase initialized");
 }
 
 /// Create a [AndroidNotificationChannel] for heads up notifications
